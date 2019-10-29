9a4771890b6b3b0663d87f500be54fbec26a4e88
borgr <ktilana@gmail.com>
2019-10-29 12:26:28 +0200

    available pos m2 exraction

--- using_m2/GEC_UD_divergences_m2.py
+++ using_m2/GEC_UD_divergences_m2.py
@@ -1,11 +1,15 @@
+import os
+import re
 import sys
-import itertools
-import json
-import pandas as pd
-import numpy as np
 from collections import Counter
-from queue import Queue
+from html import unescape
 from itertools import combinations as combs
+from queue import Queue
+from string import punctuation
+
+import numpy as np
+import pandas as pd
+import six
 
 
 def get_annotation_from_m2_russian(m2_path):
@@ -27,19 +31,19 @@ def get_annotation_from_m2_russian(m2_path):
                     index += 1
                 errors = []
             elif line[0] == 'A':
-                error_type, (start_index, end_index), correction = get_error_type_russian(
+                error_type, (start_index, end_index), correction = get_error_type_general(
                     line)
                 errors.append(
                     [error_type, (start_index, end_index), correction])
             elif line == '\n':
-                sentence_id = add_results_russian(
+                sentence_id = add_results_general(
                     errors, res, sentence, sentence_id)
                 errors = []
             line = m2_file.readline()
     return res
 
 
-def get_error_type_russian(line):
+def get_error_type_general(line):
     split_line = line.split("|||")
     start_index = int(split_line[0].split()[1])
     end_index = int(split_line[0].split()[2])
@@ -48,7 +52,7 @@ def get_error_type_russian(line):
     return (error_type, (start_index, end_index), correction)
 
 
-def add_results_russian(errors, res, sentence, sentence_id):
+def add_results_general(errors, res, sentence, sentence_id):
     """
     adds results of m2 corrections in a single sentencce
     :param errors: list of errors and their type/location
@@ -104,13 +108,12 @@ def get_annotation_from_m2(m2_path):
     res = []
     sentence_id = 1
     with open(m2_path) as m2_file:
-        line = m2_file.readline()
         errors = []
         sentence = []
-        while line:
+        for line in m2_file:
             if (line[0] == 'S'):
                 if sentence:  # its new sentence sentence
-                    sentence_id = add_results(
+                    sentence_id = add_results_errant(
                         errors, res, sentence, sentence_id)
                 words_in_sentence = line.split()[1:]
                 sentence = []  # clear previous sentence
@@ -119,28 +122,19 @@ def get_annotation_from_m2(m2_path):
                     sentence.append([words_in_sentence[index]])
                     index += 1
                 errors = []  # clear previous errors
-                line = m2_file.readline()
-                continue
             elif (line[0] == 'A'):  # error line
                 if (line[2] == '-'):  # no error actually
                     errors = []
-                    line = m2_file.readline()
-                    continue
                 else:  # actually error occurred - separate between missing, replacement, unneccessary and adjust the
                     error_type, (start_index,
-                                 end_index), correction = get_error_type(line)
+                                 end_index), correction = get_error_type_errant(line)
                     errors.append(
                         [error_type, (start_index, end_index), correction])
-                    line = m2_file.readline()
-                    continue
-            else:
-                line = m2_file.readline()
-                continue
-        add_results(errors, res, sentence, sentence_id)
+        add_results_errant(errors, res, sentence, sentence_id)
     return res
 
 
-def add_results(errors, res, sentence, sentence_id):
+def add_results_errant(errors, res, sentence, sentence_id):
     """
     adds results of m2 corrections in a single sentencce
     :param errors: list of errors and their type/location
@@ -187,7 +181,7 @@ def add_results(errors, res, sentence, sentence_id):
     return sentence_id
 
 
-def get_error_type(line):
+def get_error_type_errant(line):
     """
     gets line from M2 file and extracts error types, start and end index of error and correction
     :param line: line from file
@@ -223,8 +217,11 @@ def get_tokenized(conllu):
             if line.startswith(id_symb):
                 counter += 1
                 sent_id = line.split(id_symb)[-1]
-                sent_id = int(sent_id[0:-1])
-            # add becuase afte 10,000 sentence
+                try:
+                    sent_id = int(sent_id[0:-1])
+                except:
+                    pass
+            # add becuase after 10,000 sentences udpipe count restarts
             elif line == '\n' or (counter == 10000 and line.startswith("# newdoc")):
                 # there is #newdoc without a new line, shifting all sentences
                 assert (not res) or sent_id != res[-1][0]
@@ -236,6 +233,77 @@ def get_tokenized(conllu):
         res.append((sent_id, tokens))
     return res
 
+def preprocess_word(word):
+    """standardize word form for the alignment task"""
+    return word.strip().lower()
+
+
+def regularize_word(word):
+    """changes structure of the word to the same form (e.g. lowercase)"""
+
+    # remove non-alphanumeric
+    word = unescape(word)
+    word = word.lower()
+    pattern = re.compile(r"[^\w" + punctuation + "]+")
+    word = pattern.sub("", word)
+    return word
+
+
+def word_tokenize(s):
+    """tokenizes a sentence to words list"""
+    return re.split(r"\W", s)
+
+
+def count_combine_to_word(tokens, word):
+    i = 0
+    subword = ""
+    reg_word = regularize_word(word)
+    while reg_word.startswith(regularize_word(subword)):
+        try:
+            subword += tokens[i]
+        except:
+            raise
+        i += 1
+        if regularize_word(subword) == reg_word:
+            return i
+    return 0
+
+def retokenize(tokenized, word_lists, spare=None):
+    res_tokens = []
+    res_ids = []
+    token_id = 0
+    if len(word_lists) > 2 and word_lists[-1] == ["."] and word_lists[-2] == ["etc"]:
+        word_lists[-1] = []
+        word_lists[-2] = ["etc", "."]
+    for word_list_id, word_list in enumerate(word_lists):
+        res_tokens.append([])
+        res_ids.append([])
+        word_id = 0
+        while word_id < len(word_list):
+            word = word_list[word_id]
+            combine_id = count_combine_to_word(tokenized[token_id:], word)
+            combine_tokens_id = count_combine_to_word(
+                word_list[word_id:], tokenized[token_id])
+            if combine_id is not 0:
+                res_tokens[-1] += tokenized[token_id:token_id + combine_id]
+                res_ids[-1] += list(range(token_id + 1,
+                                          token_id + 1 + combine_id))
+                token_id += combine_id
+                word_id += 1
+            elif combine_tokens_id:
+                res_tokens[-1].append(tokenized[token_id])
+                res_ids[-1].append(token_id + 1)
+                token_id += 1
+                word_id += combine_tokens_id
+            elif spare:
+                if spare != ([], []) and spare != word_lists:
+                    return retokenize(tokenized, spare)
+                else:
+                    raise NotImplementedError
+            else:
+                print(tokenized, word_lists)
+                raise NotImplementedError
+    return res_tokens, res_ids
 
 def get_alignments(alignments, esl_tokenized, cesl_tokenized, comparison):
     """
@@ -246,8 +314,8 @@ def get_alignments(alignments, esl_tokenized, cesl_tokenized, comparison):
     :param comparison: errors and list of corrected vs. original sentences
     :return: nothing
     """
-    assert len(esl_tokenized) == len(cesl_tokenized) == len(comparison), "len 1:" + str(
-        len(esl_tokenized)) + "len2: " + str(len(cesl_tokenized)) + "len3:" + str(len(comparison))
+    assert len(esl_tokenized) == len(cesl_tokenized) == len(comparison), "len 1: " + str(
+        len(esl_tokenized)) + "len2: " + str(len(cesl_tokenized)) + "len3: " + str(len(comparison))
     for i in range(len(comparison)):
         align_dict = {}
         index = 1
@@ -451,19 +519,143 @@ def get_node_depth(node, graph):
             visited.add(neighbour)
     raise IndexError("Target node unreachable")
 
+def regularize_word(word):
+    """changes structure of the word to the same form (e.g. lowercase)"""
 
-def syntactic_m2(en, corr, alignments, confusion_dict_pos, confusion_dict_paths):
-    pass
+    # remove non-alphanumeric
+    word = unescape(word)
+    word = word.lower()
+    pattern = re.compile(r"[^\w" + punctuation + "]+")
+    word = pattern.sub("", word)
+    return word
 
 
-def get_confusion_matrix(en, corr, alignments, confusion_dict_pos, confusion_dict_paths):
-    assert len(en) == len(corr) == len(alignments), "len en: " + str(len(en)) + " len of corr: " + \
-        str(len(corr)) + " len all: " + str(len(alignments))
+def cut_tokenized_by_text(text, tokens):
+    if not text:
+        return 0
+    if not isinstance(text, six.string_types):
+        text = "".join(text)
+    i = 0
+    subword = ""
+    reg_word = regularize_word(text)
+    while reg_word.startswith(regularize_word(subword)):
+        try:
+            subword += tokens[i]
+        except Exception as e:
+            raise
+        i += 1
+        if regularize_word(subword) == reg_word:
+            return i
+    return 0
+
+
+def syntactic_m2(src, corr, m2_path, pos=True, out_path=None):
+    """
+    Converts an m2 file and parsed source and correction conllus and m2 to a syntax based m2 file
+    :param src: parsed conllu of the source (see parse_conllu for expected output)
+    :param corr: parsed conllu of the correction (see parse_conllu for expected output)
+    :param m2_path: path to an m2 file
+    :param pos: whether to use pos as edges
+    :param out_path: place to write the m2 file, if unspecificed the original m2 location will be preserved and stx extension will be added before the .m2
+    :return:
+    """
+    assert len(src) == len(corr), " len en: " + str(len(src)) + " len of corr: " + \
+                                                     str(len(corr)))
+    if not out_path:
+        out_path = os.path.splitext(m2_path)
+        out_path = "".join([out_path[0], ".stx", out_path[1]])
+    with open(out_path, "w") as outm2:
+        with open(m2_path) as m2:
+            i = -1
+            for m2_line in m2:
+                out_line = m2_line
+                if m2_line.strip().startswith("S"):
+                    i += 1
+                    assert i <= len(src)
+                    src_n, src_g = conll2graph(src[i])
+                    src_idxs = sorted([int(key) for key in src_n.keys()])
+                    src_tokens = [src_n[str(idx)]["wordform"] for idx in src_idxs]
+                    corr_n, corr_g = conll2graph(corr[i])
+                    corr_idxs = sorted([int(key) for key in corr_n.keys()])
+                    corr_tokens = [corr_n[str(idx)]["wordform"] for idx in corr_idxs]
+                    source_m2_tokens = m2_line[1:].strip().split()
+                    last_source_end = 0
+                    src_used_tok = 0
+                    corr_used_tok = 0
+                elif m2_line.strip().startswith("A") and "noop" not in m2_line:
+                    # Remember the case where error only changes spaces
+                    error_type, (start_index, end_index), corr_m2_tokens = get_error_type_general(
+                        m2_line)
+
+                    if "".join(source_m2_tokens[start_index: end_index]).strip() == "".join(corr_m2_tokens).strip() != "":
+                        print("Correction changes only non-punctuation and non-alpha characters", "".join(source_m2_tokens[start_index: end_index]), "->", "".join(corr_m2_tokens), "In sentence:", " ".join(source_m2_tokens))
+                    corr_m2_tokens = corr_m2_tokens.strip().split()
+                    # calculate start_index for source
+                    src_start_tok = cut_tokenized_by_text(source_m2_tokens[last_source_end:start_index], src_tokens[src_used_tok:]) + src_used_tok
+                    # calcualte end index for source
+                    src_end_tok = cut_tokenized_by_text(source_m2_tokens[start_index: end_index], src_tokens[src_start_tok:]) + src_start_tok
+                    # calculate start_index for correction
+                    corr_start_tok = cut_tokenized_by_text(source_m2_tokens[last_source_end:start_index], corr_tokens[corr_used_tok:]) + corr_used_tok
+                    # calcualte end index for correction
+                    corr_end_tok = cut_tokenized_by_text(corr_m2_tokens, corr_tokens[corr_start_tok:]) + corr_start_tok
+
+                    last_source_end = end_index
+                    src_used_tok = src_end_tok
+                    corr_used_tok = corr_end_tok
+
+                    if pos:
+                        if src_end_tok == src_start_tok:
+                            head = "None"
+                        else:
+                            # add 1 as conll starts counting from 1
+                            head = str(highest_or_none([str(idx) for idx in range(src_start_tok + 1, src_end_tok + 1)],
+                                                       src_g))  # TODO check how it works (including when multiple source words
+                        if corr_end_tok == corr_start_tok:
+                            tail = "None"
+                        else:
+                            # add 1 as conll starts counting from 1
+                            tail = str(highest_or_none([str(idx) for idx in range(corr_start_tok + 1, corr_end_tok + 1)],
+                                                       corr_g))
+
+                        # Skip technical additional nodes
+                        if "." in head:
+                            continue
+                        if head == "None":
+                            src_pos = "None"
+                        else:
+                            src_pos = src_n[head]["pos"]
+
+                        if tail == "None":
+                            corr_pos = "None"
+                        else:
+                            corr_pos = corr_n[tail]["pos"]
+                        if head == tail == "None":
+                            if start_index == end_index and not corr_m2_tokens:
+                                print("Correction replaces empty span by an empty correction", " ".join(source_m2_tokens))
+                            else:
+                                print("Warning: both head and tail in error is None in sentence:", " ".join(source_m2_tokens))
+
+                        syntactic_error = src_pos + "->" + corr_pos
+                        out_line = m2_line.split("|||")
+                        out_line[1] = syntactic_error
+                        out_line = "|||".join(out_line)
+                    else:
+                        raise NotImplementedError # edge m2
+                elif "noop" in m2_line:
+                    pass
+                elif m2_line.strip():
+                    raise ValueError("Unexpected line, m2 lines should start with A S or be blank")
+                outm2.write(out_line)
+
+
+def get_confusion_matrix(src, corr, alignments, confusion_dict_pos, confusion_dict_paths):
+    assert len(src) == len(corr) == len(alignments), "len en: " + str(len(src)) + " len of corr: " + \
+                                                     str(len(corr)) + " len all: " + str(len(alignments))
 
     def strip_direction(x): return x.split("_")[0]
 
-    for i in range(len(en)):
-        en_n, en_g = conll2graph(en[i])
+    for i in range(len(src)):
+        src_n, src_g = conll2graph(src[i])
         corr_n, corr_g = conll2graph(corr[i])
         alignment = alignments[i]
         # Simplify the alignment to a set of one-to-one pairs
@@ -482,27 +674,27 @@ def get_confusion_matrix(en, corr, alignments, confusion_dict_pos, confusion_dic
             if "." in head:
                 continue
             try:
-                en_pos = en_n[head]["pos"]
+                src_pos = src_n[head]["pos"]
             except KeyError:
-                print(i, en[i])
+                print(i, src[i])
                 continue
             if tail == "None":
                 corr_pos = "None"
             else:
                 corr_pos = corr_n[tail]["pos"]
-            if en_pos not in confusion_dict_pos:
-                confusion_dict_pos[en_pos] = Counter()
-            confusion_dict_pos[en_pos][corr_pos] += 1
+            if src_pos not in confusion_dict_pos:
+                confusion_dict_pos[src_pos] = Counter()
+            confusion_dict_pos[src_pos][corr_pos] += 1
         # Path confusion dict
         for pair in combs(one_to_one, 2):
-            (en_head, corr_head), (en_tail, corr_tail) = pair
+            (src_head, corr_head), (src_tail, corr_tail) = pair
             # Skip technical additional nodes
             if "." in head:
                 continue
-            en_path_arr = get_path(en_head, en_tail, en_g)
-            if len(en_path_arr) > 1:
+            src_path_arr = get_path(src_head, src_tail, src_g)
+            if len(src_path_arr) > 1:
                 continue
-            en_path = strip_direction(en_path_arr[0])
+            src_path = strip_direction(src_path_arr[0])
             if corr_head == corr_tail:
                 corr_path = "Nodes collapsed"
             elif corr_head == "None" and corr_tail == "None":
@@ -512,9 +704,9 @@ def get_confusion_matrix(en, corr, alignments, confusion_dict_pos, confusion_dic
             else:
                 corr_path_arr = get_path(corr_head, corr_tail, corr_g)
                 corr_path = "->".join(list(map(strip_direction, corr_path_arr)))
-            if en_path not in confusion_dict_paths:
-                confusion_dict_paths[en_path] = Counter()
-            confusion_dict_paths[en_path][corr_path] += 1
+            if src_path not in confusion_dict_paths:
+                confusion_dict_paths[src_path] = Counter()
+            confusion_dict_paths[src_path][corr_path] += 1
 
 
 def extract_matrices(confusion_dict_paths, confusion_dict_pos, data_name):
@@ -616,13 +808,14 @@ def main():
         comparison = get_annotation_from_m2(m2_path)
         alignments = []  # will be a list of dictionaries
         get_alignments(alignments, esl_tokenized, cesl_tokenized, comparison)
-        en = parse_conllu(conllu_path)
+        src = parse_conllu(conllu_path)
         corr = parse_conllu(conllu_path_corrected)
-        assert len(en) == len(corr) == len(alignments), "len en: " + str(len(en)) + " len of corr: " + \
+        assert len(src) == len(corr) == len(alignments), "len src: " + str(len(src)) + " len of corr: " + \
             str(len(corr)) + " len all: " + str(len(alignments))
         confusion_dict_paths = {}
         confusion_dict_pos = {}
-        get_confusion_matrix(en, corr, alignments,
+        syntactic_m2(src, corr, m2_path)
+        get_confusion_matrix(src, corr, alignments,
                              confusion_dict_pos, confusion_dict_paths)
         extract_matrices(confusion_dict_paths,
                          confusion_dict_pos, conllu_path.split('/')[-1])
